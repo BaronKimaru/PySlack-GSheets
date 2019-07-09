0d3470d48532fe47df64ee43d8467c5120cec644
BaronKimaru <bizzinali@gmail.com>
2019-07-09 12:11:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,35 @@
-debug = True
+class Config:
+	"""Common Configurations"""
+	DEBUG = False
+	CSRF_ENABLED = True
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+	"""Configurations for Development Environment"""
+	DEBUG = True
+
+
+class TestingConfig(Config):
+	"""Configurations for Testing Environment, with a separate database"""
+	DEBUG = True
+	TESTING = True
+
+
+class StagingConfig(Config):
+	"""Configurations for Staging Environment"""
+	DEBUG = True
+
+
+class ProductionConfig(Config):
+	"""Configuration for Production Environment"""
+	DEBUG = False
+	TESTING = False
+
+app_config = {
+	'development': DevelopmentConfig,
+	'testing': TestingConfig,
+	'staging': StagingConfig,
+	'production': ProductionConfig,
+}
+
